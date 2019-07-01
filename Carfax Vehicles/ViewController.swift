1a742bd645a71d37532961d8b492b5594db15b60
Edgar Delgado <delga157@mail.chapman.edu>
2019-07-01 14:03:32 -0700

    Initial Commit

---
+++ Carfax Vehicles/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Carfax Vehicles
+//
+//  Created by Edgar Delgado on 7/1/19.
+//  Copyright © 2019 Edgar Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("View did load")
+    }
+
+
+}
+
